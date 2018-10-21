e119ce6ee76970b33bd9470790785f2e251e78ed
Andrew Head <head.andrewm@gmail.com>
2018-10-20 19:45:48 -0700

    Updated getPlugins script to get relevant Android versions

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,2 +1,2 @@
-git clone git://git.jetbrains.org/idea/android.git android
-git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+git clone --depth=1 -b idea/182.4892.20 git://git.jetbrains.org/idea/android.git android
+git clone --depth=1 -b idea/182.4892.20 git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
